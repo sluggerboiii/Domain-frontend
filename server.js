4092697a72d8f7092c896d49db63e1c10b1ae833
sluggerboiii <sluggerboiii@gmail.com>
2025-04-29 00:33:59 +0530

    React frontend project initial commit

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
